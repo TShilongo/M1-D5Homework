f98eeabb2836c3f8f2d7002e87bad08d707e6143
TShilongo <tislashilongo03@gmail.com>
2022-03-04 16:48:47 +0100

    Answer to exercise 6

--- index.js
+++ index.js
@@ -78,20 +78,35 @@ console.log(boundaryValue)
  It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
 */
 
-/* WRITE YOUR ANSWER HERE */
+
+/* WRITE YOUU ANSWER HERE */
 
 /* EXERCISE 6
  Write a function called "check3and7" which accepts a positive number as a parameter and checks if it is a multiple of 3 or a multiple of 7.
  HINT: Modulus Operator
 */
 
-/* WRITE YOUR ANSWER HERE */
+function check3and7(no) {
+    if (no % 3 === 0 || no % 7 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+let checkSum = check3and7(6)
+
+console.log(checkSum)
+
+
 
 /* EXERCISE 7
  Write a function called "reverseString" which programmatically reverses a given string (es.: Strive => evirtS).
 */
 
-/* WRITE YOUR ANSWER HERE */
+
+
+
 
 /* EXERCISE 8
  Write a function called "upperFirst" which capitalizes the first letter of each word of a given string passed as a parameter.
